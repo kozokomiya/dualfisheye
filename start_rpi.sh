815fbdb0e135b26e65b110bf9acbb983b111656a
Kozo Komiya <kozo.komiya@gmail.com>
2020-12-19 01:19:14 +0000

    add rpi script.sh

---
+++ start_rpi.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export GST_PLUGIN_PATH="./build:${GST_PLUGIN_PATH}"
+export GST_DEBUG="1,dualfisheye:4"
+# DEBUG_LEVEL  1=ERROR, 2=WARNING, 3=FIXME, 4=INFO, 5=DEBUG, 6=LOG, 7=TRACE
+
+# force cache clear
+rm -rf ~/.cache/gstreamer-1.0
+
+# Launch dualfisheye conversion
+gst-launch-1.0 -v \
+  v4l2src device=/dev/video0 !\
+  omxmjpegdec !\
+  videoconvert !\
+  dualfisheye !\
+  videoconvert !\
+  autovideosink
