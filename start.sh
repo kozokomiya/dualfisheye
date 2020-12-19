256897f5c3723139fe16ef099e35ea1d8b760b50
Kozo Komiya <kozo.komiya@gmail.com>
2020-12-19 11:25:44 +0900

    update scripts

--- start.sh
+++ start.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
 if [ "$(uname)" == 'Darwin' ]; then   # Mac Case
-  export CAMERA='avfvideosrc device-index=1 !'
+  export CAMERA_SRC='avfvideosrc device-index=0 !'
 elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then  # Linux Case
   if [[ $(uname -a) == *raspberry* ]]; then   # Raspberry Pi case
-    export CAMERA='v4l2src device=/dev/video0 ! omxmjpegdec !'
-  else
-    export CAMERA='v4l2src device=/dev/video0 ! jpegdec !'
+    export CAMERA_SRC='v4l2src device=/dev/video0 ! omxmjpegdec !'
+  else                                        # Linux PC 
+    export CAMERA_SRC='v4l2src device=/dev/video0 ! jpegdec !'
   fi
 else
   echo 'Your platform is not supported. Only for linux or Mac.'
   exit 1
 fi
-  
-
-echo $CAMERA
 
 export GST_PLUGIN_PATH="./build:${GST_PLUGIN_PATH}"
 export GST_DEBUG="1,dualfisheye:4"
@@ -25,7 +22,7 @@ rm -rf ~/.cache/gstreamer-1.0
 
 # Launch dualfisheye conversion
 gst-launch-1.0 -v \
-  $CAMERA \
+  $CAMERA_SRC \
   videoconvert !\
   dualfisheye !\
   videoconvert !\
